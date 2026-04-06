33cf5445d87c7f6567d09993712a7701bae8b709
Pranav Patel <pranav742001@gmail.com>
2026-04-06 11:07:21 +0530

    Update folder

---
+++ Interview_prep/fresher.py
@@ -0,0 +1,37 @@
+# # arrays
+
+# arr = [1 , 2 , "Pranav" , 3.14 , True]
+#      # 0 ,  1 ,    2 ,      3 ,  4.   indixing
+
+# print(arr[1])
+
+# arr[2] = 3
+# print(arr)
+
+# arr[4] = 4
+# print(arr)
+
+
+# arr = [1,2,3,4]
+
+# value = int(input("Enter value:"))
+
+# arr.append(value)
+# print(arr)
+
+
+
+# arr = [1,2,3,4,5]
+
+# arr.insert(5,6)#index 5 , value 6
+# print(arr)
+
+
+
+arr = [1,2,3,4]
+
+arr.remove(2) #removes the number 2 from the array
+
+arr.pop() #removes the last element from the array
+
+print(arr)
